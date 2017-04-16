aa90cea390238f197136b910288e7fe188b5a899
Connor Abbott <cwabbott0@gmail.com>
2017-04-15 22:40:57 -0400

    add a compile option to dump GLSL shaders

--- microc.ml
+++ microc.ml
@@ -1,11 +1,19 @@
 (* Top-level of the MicroC compiler: scan & parse the input,
    check the resulting AST, generate LLVM IR, and dump the module *)
 
-type action = Ast | LLVM_IR | Compile
+module StringMap = Map.Make(String)
+
+type action = Ast | LLVM_IR | GLSL | Compile
+
+let print_glsl glsl =
+  StringMap.iter (fun name glsl ->
+    print_string ("glsl for " ^ name ^ ":\n\n");
+    print_string glsl) glsl
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);	(* Print the AST only *)
+                              ("-g", GLSL);     (* Only generate GLSL *)
 			      ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
 			      ("-c", Compile) ] (* Generate, check LLVM IR *)
   else Compile in
@@ -14,6 +22,7 @@ let _ =
   let sast = Semant.check ast in
   match action with
     Ast -> print_string (Sast.string_of_sprogram sast)
+  | GLSL -> print_glsl (Glslcodegen.translate sast)
   | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
   | Compile -> let m = Codegen.translate sast in
     Llvm_analysis.assert_valid_module m;
