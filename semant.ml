1372e10578c48e1646ea9e0af8206bd132beef02
Connor Abbott <cwabbott0@gmail.com>
2017-05-09 02:42:30 -0400

    add support for comparing ivec's

--- semant.ml
+++ semant.ml
@@ -544,8 +544,10 @@ let check program =
                      -> (Mat(Int, 1, l), IDiv)
           | Mod,    Mat(Int, 1, l),   Mat(Int, 1, l') when l = l'
                      -> (Mat(Int, 1, l), IMod)
-          | Equal,   Mat(Int, 1, 1),   Mat(Int, 1, 1)   -> (Mat(Bool, 1, 1), IEqual)
-          | Neq,     Mat(Int, 1, 1),   Mat(Int, 1, 1)   -> (Mat(Bool, 1, 1), INeq)
+          | Equal,   Mat(Int, 1, l),   Mat(Int, 1, l') when l = l'
+                     -> (Mat(Bool, 1, 1), IEqual)
+          | Neq,     Mat(Int, 1, l),   Mat(Int, 1, l') when l = l'
+                     -> (Mat(Bool, 1, 1), INeq)
           | Add,     Mat(Float, w, l), Mat(Float, w', l') when w = w' && l = l'
                      -> (Mat(Float, w, l), FAdd)
           | Sub,     Mat(Float, w, l), Mat(Float, w', l') when w = w' && l = l' 
